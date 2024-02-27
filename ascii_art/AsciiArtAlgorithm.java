ecd2d584f635e1f3aca3ee2fb6c7b3cff8ac4032
danif64 <danif64@gmail.com>
2024-02-27 14:15:40 +0200

    IMPLEMENTED algorithm

--- ascii_art/AsciiArtAlgorithm.java
+++ ascii_art/AsciiArtAlgorithm.java
@@ -1,13 +1,46 @@
 package ascii_art;
 
+import image.DimensionException;
+import image.Image;
+import image.SubImageList;
+import image_char_matching.CharConverter;
+import image_char_matching.SubImgCharMatcher;
+
 public class AsciiArtAlgorithm {
 
 
-    public AsciiArtAlgorithm() {
+    private Image image;
+    private int resolution;
+    private char [] asciiChars;
+    private final SubImgCharMatcher subImgCharMatcher;
+
+
+
+    public AsciiArtAlgorithm(Image image, int resolution, char [] asciiChars) throws DimensionException {
+        if (image.getWidth() % resolution != 0 || image.getHeight() % resolution != 0) {
+            throw new DimensionException();
+        }
+        this.image = image;
+        this.resolution = resolution;
+        this.asciiChars = asciiChars;
+        this.subImgCharMatcher = new SubImgCharMatcher(asciiChars);
     }
 
-    public char [] [] run(){
-        return null;
+    public char [] [] run() throws DimensionException {
+
+        // convert to subimagelist
+
+        char [] [] res = new char[resolution][resolution];
+        SubImageList subImageList = new SubImageList(image, resolution); // todo: is necessary to use list (instead of stack)?
+
+        for (int i = 0; i < resolution; i++) {
+            for (int j = 0; j < resolution; j++) {
+                double averageSubImage = subImageList.get(i * resolution + j).getAverageBrightness();
+                res[i][j] = subImgCharMatcher.getCharByImageBrightness(averageSubImage);
+            }
+        }
+        return res;
+
     }
 
 }
--- image/SubImageList.java
+++ image/SubImageList.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class SubImageList extends ArrayList<SubImage> {
 
 
-    public SubImageList(Image image, int resolution) throws DimensionExeption {
+    public SubImageList(Image image, int resolution) throws DimensionException {
         super();
         // todo: check if image is a integral multiple of 2
         if (image.getWidth() % resolution != 0 || image.getHeight() % resolution != 0) {
-            throw new DimensionExeption();
+            throw new DimensionException();
         }
         for (int i = 0; i < image.getHeight(); i += resolution) { //todo is resolution or image/resolution
             for (int j = 0; j < image.getWidth(); j += resolution) {
